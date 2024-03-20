b2912c813e2acc8f52ead2ec15ffacfdabc5e47a
drew <andrewhorndeborba@gmail.com>
2024-03-20 08:36:17 -0300

    Inicio do projeto

---
+++ js/main.js
@@ -0,0 +1,105 @@
+//classie
+( function( window ) {
+
+	'use strict';
+	
+	// class helper functions from bonzo https://github.com/ded/bonzo
+	
+	function classReg( className ) {
+	  return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+	}
+	
+	var hasClass, addClass, removeClass;
+	
+	if ( 'classList' in document.documentElement ) {
+	  hasClass = function( elem, c ) {
+		return elem.classList.contains( c );
+	  };
+	  addClass = function( elem, c ) {
+		elem.classList.add( c );
+	  };
+	  removeClass = function( elem, c ) {
+		elem.classList.remove( c );
+	  };
+	}
+	else {
+	  hasClass = function( elem, c ) {
+		return classReg( c ).test( elem.className );
+	  };
+	  addClass = function( elem, c ) {
+		if ( !hasClass( elem, c ) ) {
+		  elem.className = elem.className + ' ' + c;
+		}
+	  };
+	  removeClass = function( elem, c ) {
+		elem.className = elem.className.replace( classReg( c ), ' ' );
+	  };
+	}
+	
+	function toggleClass( elem, c ) {
+	  var fn = hasClass( elem, c ) ? removeClass : addClass;
+	  fn( elem, c );
+	}
+	
+	var classie = {
+	  // full names
+	  hasClass: hasClass,
+	  addClass: addClass,
+	  removeClass: removeClass,
+	  toggleClass: toggleClass,
+	  // short names
+	  has: hasClass,
+	  add: addClass,
+	  remove: removeClass,
+	  toggle: toggleClass
+	};
+	
+	// transport
+	if ( typeof define === 'function' && define.amd ) {
+	  define( classie );
+	} else {
+	  window.classie = classie;
+	}
+	
+	})( window );
+	
+// nav
+(function() {
+
+	var bodyEl = document.body,
+		content = document.querySelector( '.content-wrap' ),
+		openbtn = document.getElementById( 'open-button' ),
+		closebtn = document.getElementById( 'close-button' ),
+		isOpen = false;
+
+	function init() {
+		initEvents();
+	}
+
+	function initEvents() {
+		openbtn.addEventListener( 'click', toggleMenu );
+		if( closebtn ) {
+			closebtn.addEventListener( 'click', toggleMenu );
+		}
+
+		content.addEventListener( 'click', function(ev) {
+			var target = ev.target;
+			if( isOpen && target !== openbtn ) {
+				toggleMenu();
+			}
+		} );
+	}
+
+	function toggleMenu() {
+		if( isOpen ) {
+			classie.remove( bodyEl, 'show-menu' );
+		}
+		else {
+			classie.add( bodyEl, 'show-menu' );
+		}
+		isOpen = !isOpen;
+	}
+
+	init();
+
+})();
