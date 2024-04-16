cc2975f4a4955fca2c2ed3e7b08019fee7f5cc05
Andrew Horde de Borba <161035279+drewhdb@users.noreply.github.com>
2024-04-16 20:01:37 -0300

    Update banco projeto.sql

--- sql/banco projeto.sql
+++ sql/banco projeto.sql
@@ -4,119 +4,144 @@ USE projeto;
 
 -- Tabela de usuários para acesso ao sistema
 CREATE TABLE usr (
-    USRID INT PRIMARY KEY AUTO_INCREMENT,
-    USRNOME VARCHAR(30),
-    USRSENHA VARCHAR(20),
-    USRCRIACAO TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    USRATIVO INT DEFAULT 1
+    usrid INT PRIMARY KEY AUTO_INCREMENT,
+    usrnome VARCHAR(30),
+    usrsenha VARCHAR(20),
+    usrcriacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    usrativo INT DEFAULT 1
 )  ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-insert into usr (USRNOME, USRSENHA) values ('Andrew', 'senha'), ('Luan', 'senha'), ('Braian', 'senha');
+INSERT INTO usr (usrnome, usrsenha) VALUES ('Andrew', 'senha'), ('Luan', 'senha'), ('Braian', 'senha');
 
 -- Tabela de categorias dos módulos
 CREATE TABLE ctm (
-    CTMID INT PRIMARY KEY AUTO_INCREMENT,
-    CTMDESC VARCHAR(10) NOT NULL,
-    CTMSUBMDL DECIMAL(1,0) DEFAULT 0
+    ctmid INT PRIMARY KEY AUTO_INCREMENT,
+    ctmdesc VARCHAR(10) NOT NULL,
+    ctmsubmdl DECIMAL(1,0) DEFAULT 0,
+    ctmalexa DECIMAL(1,0) DEFAULT 0
 )  ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO ctm (CTMDESC) VALUES ('ERP'), ('WHATSAPP'), ('SUB-MODULO');
+-- Tabela de categorias dos módulos
+INSERT INTO ctm (ctmdesc) VALUES ('ERP'), ('WHATSAPP');
+INSERT INTO ctm (ctmdesc, ctmsubmdl) VALUES ('SUB-MODULO', 1);
+INSERT INTO ctm (ctmdesc, ctmalexa) VALUES ('ALEXA', 1);
 
 -- Tabela de módulos
 CREATE TABLE mdl (
-    MDLMODULO INT PRIMARY KEY,
-    MDLDESC VARCHAR(30),
-    MDLCATEGORIA INT,
-    CONSTRAINT fk_mod_ctm FOREIGN KEY (MDLCATEGORIA) REFERENCES ctm (CTMID),
+    mdlmodulo VARCHAR(20) PRIMARY KEY,
+    mldesc VARCHAR(100),
+    mdlcategoria INT,
+    CONSTRAINT fk_mod_ctm FOREIGN KEY (mdlcategoria) REFERENCES ctm (ctmid)
 )  ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO mdl (mdlmodulo, mldesc, mdlcategoria) VALUES ('GESTÃO', 'sistema ERP de vendas', '1'),
+    ('PLACE', 'sistema ERP de vendas', '1'),
+    ('MEMO', 'sistema ERP de vendas', '1'),
+    ('PV', 'sistema ERP de vendas', '1'),
+    ('WHATSAPP', 'automação whatsapp', '2'),
+    ('OUTDOOR', 'GESTÃO PARA TABLET', '3'),
+    ('ORYXNFE', 'PLACE PARA CELULAR', '3'),
+    ('BI', 'Gráficos de dados', '3'),
+    ('GERTEC', 'LEITURA DE CÓDIGO DE BARRA PARA OS CLIENTES DOS CLIENTES', '3'),
+    ('ALEXA', 'integração Alexa', '4');
+
 -- DO SUPPORT
-DROP TABLE IF EXISTS ver;
-CREATE TABLE  ver (
-  VERVERSAO int(11) AUTO_INCREMENT,
-  VERMODULO INT,
-  VERNOME char(15) DEFAULT '',
-  VERLIBERAC datetime DEFAULT '1899-12-30 00:00:00',
-  VERVALIDAD datetime DEFAULT '1899-12-30 00:00:00',
-  CONSTRAINT fk_ver PRIMARY KEY (VERVERSAO, VERMODULO)
+CREATE TABLE ver (
+  verversao INT AUTO_INCREMENT,
+  vermodulo INT,
+  vernome CHAR(15) DEFAULT '',
+  verliberac DATETIME DEFAULT '1899-12-30 00:00:00',
+  vervalidad DATETIME DEFAULT '1899-12-30 00:00:00',
+  CONSTRAINT fk_ver PRIMARY KEY (verversao, vermodulo)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 -- Tabela de clientes principais
 CREATE TABLE cl1 (
-    CL1CLIENTE INT PRIMARY KEY AUTO_INCREMENT,
-    CL1NOME VARCHAR(50)
+    cl1cliente INT PRIMARY KEY AUTO_INCREMENT,
+    cl1nome VARCHAR(50)
 )  ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Tabela de tipos de estabelecimentos
 CREATE TABLE cl4 (
-    CL4TIPO INT PRIMARY KEY AUTO_INCREMENT,
-    CL4DESC VARCHAR(20) NOT NULL
+    cl4tipo INT PRIMARY KEY AUTO_INCREMENT,
+    cl4desc VARCHAR(20) NOT NULL
 )  ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO cl4 (CL4DESC) VALUES ('Fabrica'), ('Loja'), ('Matriz');
+INSERT INTO cl4 (cl4desc) VALUES ('Fabrica'), ('Loja'), ('Matriz');
 
 -- Tabela de shoppings
 CREATE TABLE cl5 (
-    CL5SHOPPING INT PRIMARY KEY AUTO_INCREMENT,
-    CL5APELIDO VARCHAR(10) DEFAULT '',
-    CL5DESC VARCHAR(40) NOT NULL,
-    CL5ATACADO INT DEFAULT 0,
-    CL5LOCAL VARCHAR(30) DEFAULT ''
+    cl5shopping INT PRIMARY KEY AUTO_INCREMENT,
+    cl5apelido VARCHAR(10) DEFAULT '',
+    cl5desc VARCHAR(40) NOT NULL,
+    cl5atacado INT DEFAULT 0,
+    cl5local VARCHAR(30) DEFAULT ''
 )  ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO cl5 (CL5APELIDO, CL5DESC, CL5ATACADO) VALUES 
-('CCF', 'Centro de Compras Farroupilha', 0), 
-('FAC', 'Farroupilhas center', 1), 
-('585', '', 1), 
-('CCB', 'Centro de Compras Bom Principio', 0), 
+INSERT INTO cl5 (cl5apelido, cl5desc, cl5atacado) VALUES
+('CCF', 'Centro de Compras Farroupilha', 0),
+('FAC', 'Farroupilhas center', 1),
+('585', '', 1),
+('CCB', 'Centro de Compras Bom Principio', 0),
 ('Litoral', 'Shopping Litoral SC', 0);
 
 -- Tabela de estabelecimentos, matriz ou filiais de um cliente
 CREATE TABLE cl2 (
-    CL2EST INT PRIMARY KEY AUTO_INCREMENT,
-    CL2CLIENTE INT,
-    CL2TIPOEST INT,
-    CL2SHOPPING INT,
-    CL2CNPJ VARCHAR(18) DEFAULT '',
-    CL2FANTASIA VARCHAR(50) NOT NULL,
-    CL2GERENTE VARCHAR(50) DEFAULT '',
-    CL2CTT VARCHAR(15) DEFAULT '',
-    CL2OBS LONGTEXT,
-    CL2ACESSO LONGTEXT,
-    CL2ENDER LONGTEXT,
-    CL2FIM TIMESTAMP,
-    CL2INICIO TIMESTAMP,
-    CONSTRAINT fk_cl2_cl1 FOREIGN KEY (CL2CLIENTE) REFERENCES cl1 (CL1CLIENTE),
-    CONSTRAINT fk_cl2_cl4 FOREIGN KEY (CL2TIPOEST) REFERENCES cl4 (CL4TIPO),
-    CONSTRAINT fk_cl2_cl5 FOREIGN KEY (CL2SHOPPING) REFERENCES cl5 (CL5SHOPPING)
+    cl2est INT PRIMARY KEY AUTO_INCREMENT,
+    cl2cliente INT,
+    cl2tipoest INT,
+    cl2shopping INT,
+    cl2cnpj VARCHAR(18) DEFAULT '',
+    cl2fantasia VARCHAR(50) NOT NULL,
+    cl2gerente VARCHAR(50) DEFAULT '',
+    cl2ctt VARCHAR(15) DEFAULT '',
+    cl2obs LONGTEXT,
+    cl2acesso LONGTEXT,
+    cl2ender LONGTEXT,
+    cl2fim TIMESTAMP,
+    cl2inicio TIMESTAMP,
+    CONSTRAINT fk_cl2_cl1 FOREIGN KEY (cl2cliente) REFERENCES cl1 (cl1cliente),
+    CONSTRAINT fk_cl2_cl4 FOREIGN KEY (cl2tipoest) REFERENCES cl4 (cl4tipo),
+    CONSTRAINT fk_cl2_cl5 FOREIGN KEY (cl2shopping) REFERENCES cl5 (cl5shopping)
 )  ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Tabela de módulos e versões dos estabelecimentos dos clientes
 CREATE TABLE cl3 (
-    CL3EST INT,
-    CL3MODULO INT,
-    CL3VERSAO int(11) NOT NULL,
-    CL3ATUALIZ VARCHAR(10) DEFAULT '',
-    CL3RESPATT INT DEFAULT NULL,
-    CL3RESP INT NOT NULL,
-    PRIMARY KEY (CL3EST, CL3MODULO),
-    CONSTRAINT fk_cl3_cl2 FOREIGN KEY (CL3EST) REFERENCES cl2 (CL2EST),
-    CONSTRAINT fk_cl3_mdl FOREIGN KEY (CL3MODULO) REFERENCES mdl (MDLMODULO),
-    CONSTRAINT fk_cl3_ver FOREIGN KEY (CL3VERSAO) REFERENCES ver (VERVERSAO),
-    CONSTRAINT fk_cl3_responsavel FOREIGN KEY (CL3RESP) REFERENCES usr (USRID),
-    CONSTRAINT fk_cl3_responsavel_atualizacao FOREIGN KEY (CL3RESPATT) REFERENCES usr (USRID)
+    cl3est INT,
+    cl3modulo VARCHAR(20),
+    cl3versao INT(11) NOT NULL,
+    cl3atualiz VARCHAR(10) DEFAULT '',
+    cl3respatt INT DEFAULT NULL,
+    cl3resp INT NOT NULL,
+    PRIMARY KEY (cl3est, cl3modulo),
+    CONSTRAINT fk_cl3_cl2 FOREIGN KEY (cl3est) REFERENCES cl2 (cl2est),
+    CONSTRAINT fk_cl3_mdl FOREIGN KEY (cl3modulo) REFERENCES mdl (mdlmodulo),
+    CONSTRAINT fk_cl3_ver FOREIGN KEY (cl3versao) REFERENCES ver (verversao),
+    CONSTRAINT fk_cl3_responsavel FOREIGN KEY (cl3resp) REFERENCES usr (usrid),
+    CONSTRAINT fk_cl3_responsavel_atualizacao FOREIGN KEY (cl3respatt) REFERENCES usr (usrid)
+)  ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- histórico do cliente
+CREATE TABLE cl6 (
+    cl6id INT PRIMARY KEY AUTO_INCREMENT,
+    cl6est INT,
+    cl6desc VARCHAR(50),
+    cl6data DATETIME,
+    cl6resp INT,
+    CONSTRAINT fk_cl6_responsavel FOREIGN KEY (cl6resp) REFERENCES usr (usrid),
+    CONSTRAINT fk_cl6_cl2 FOREIGN KEY (cl6est) REFERENCES cl2 (cl2est)
 )  ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Tabela de anotações
 CREATE TABLE nta (
-    NTAID INT PRIMARY KEY AUTO_INCREMENT,
-    NTATITULO VARCHAR(20) NOT NULL,
-    TNAINTRO VARCHAR(60) DEFAULT '',
-    NTACONTEUDO LONGTEXT,
-    NTACRIADOR INT NOT NULL,
-    NTACRIACAO TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    NTALINK DECIMAL(1,0) DEFAULT 0,
-    NTALINKDIR VARCHAR(250) DEFAULT '#',
-    CONSTRAINT fk_nta_criador FOREIGN KEY (NTACRIADOR) REFERENCES usr (USRID)
+    ntaid INT PRIMARY KEY AUTO_INCREMENT,
+    ntatitulo VARCHAR(20) NOT NULL,
+    tnaintro VARCHAR(60) DEFAULT '',
+    ntaconteudo LONGTEXT,
+    ntacriador INT NOT NULL,
+    ntacriacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    ntalink DECIMAL(1,0) DEFAULT 0,
+    ntalinkdir VARCHAR(250) DEFAULT '#',
+    CONSTRAINT fk_nta_criador FOREIGN KEY (ntacriador) REFERENCES usr (usrid)
 )  ENGINE=InnoDB DEFAULT CHARSET=latin1;
